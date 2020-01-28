edfe2461bab836ad20c54d5473cb2e819afdcf58
sapucaialuann <sapucaialuann@id.uff.br>
2020-01-28 17:57:22 -0300

    formulários aprimorados e responsivos

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $("#show_hide_password a").on('click', function(event) {
-        event.preventDefault();
+    $("#show_hide_password a").on('click', function(event) { event.preventDefault();
         if($('#show_hide_password input').attr("type") == "text"){
             $('#show_hide_password input').attr('type', 'password');
             $('#show_hide_password i').addClass( "fa-eye-slash" );
